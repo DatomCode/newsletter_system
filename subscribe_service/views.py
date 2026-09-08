8f40567be66064e4154d68ed150bc40dc90902bb
Datom-code <enochgbadebo007@gmail.com>
2026-09-08 19:02:15 -0400

    Implement SubscribeView for newsletter subscription handling

--- subscribe_service/views.py
+++ subscribe_service/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from .serializers import NewsletterSubscriberSerializer
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+class SubscribeView(APIView):
+    permission_classes =[AllowAny]
+
+    def post(self, request):
+        serializer = NewsletterSubscriberSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"Susscessfully Subscribed to newsletter"}, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
